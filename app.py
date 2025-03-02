bc6fef93674d5c3efc3634e866135ec4169457df
karanDevX11 <bistakaran89@gmail.com>
2025-03-02 22:53:03 +0545

    Add comment

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def run_script():
     try:
-        result = subprocess.run(['python', 'webscancrawler.py', 'http://nabinkhadka1.com.np'], capture_output=True, text=True)
+        result = subprocess.run(['python', 'webscancrawler.py', 'http://nabinkhadka1.com.np'], capture_output=True, text=True)   # http://nabinkhadka1.com.np  i was looking at this guy portfolio let's see how many security vulnerabilities he have
         return f"<pre>Script output:\n{result.stdout}</pre>"
     except Exception as e:
         return f"Error: {str(e)}"
