6c5af4f457efdb8140b522e3fff98fba5df5d598
karanDevX11 <bistakaran89@gmail.com>
2025-03-23 19:06:38 +0545

    Add container and update css with animations

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 import subprocess
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/scan', methods=['POST'])
 def run_script():
+    url = request.form.get('url')
     try:
-        result = subprocess.run(['python', 'webscancrawler.py', 'http://nabinkhadka1.com.np'], capture_output=True, text=True)   # http://nabinkhadka1.com.np  i was looking at this guy portfolio let's see how many security vulnerabilities he have
+        result = subprocess.run(['python', 'webscancrawler.py', url], capture_output=True, text=True)   # http://nabinkhadka1.com.np  i was looking at this guy portfolio let's see how many security vulnerabilities he have
         return f"<pre>Script output:\n{result.stdout}</pre>"
     except Exception as e:
         return f"Error: {str(e)}"
